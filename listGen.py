78a1d88a9c45b459a89f2631439598aa3c86c257
RUZEN <nachiketa3299@gmail.com>
2022-07-28 10:36:40 +0900

    저장소를 Insertion-sort only로 바꿈

--- listGen.py
+++
@@ -1,4 +0,0 @@
-import random
-
-def generateRandomIntList(size, min_max):
-    return [random.randrange(min_max[0], min_max[1]) for _ in range(0, size)] 
--- printData.py
+++
@@ -1,6 +0,0 @@
-def printData(list, sorted=False):
-    if sorted == True:
-        print("> Sorted Data")
-    else:
-        print("> Unsorted Data")
-    print(list)
